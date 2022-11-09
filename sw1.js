0cdfc4c76fd3e1e300b79630a784c85ed67162fe
saamitraut <saamitraut@gmail.com>
2022-11-09 18:30:49 +0530

    banner added

--- sw1.js
+++ sw1.js
@@ -4,10 +4,44 @@ self.addEventListener("push", (event) => {
   //   console.log(res2.body);
   var options = {
     body: res2.body,
-    icon: "images/example.png",
+    icon: "images/mozilla-firefox-icon-logo-png-3.png",
     vibrate: [100, 50, 100],
+    image: "images/banner3.jpg",
+    actions: [
+      {
+        action: "coffee-action",
+        title: "Coffee",
+        // icon: "images/cofee.png",
+      },
+    ],
     data: { primaryKey: 1 },
   };
 
   event.waitUntil(self.registration.showNotification(res2.msg, options));
+});
+
+self.addEventListener("notificationclick", function (event) {
+  if (!event.action) {
+    // Was a normal notification click
+    console.log("Notification Click.");
+    return;
+  }
+
+  switch (event.action) {
+    case "coffee-action":
+      console.log("User ❤️️'s coffee.");
+      break;
+    case "doughnut-action":
+      console.log("User ❤️️'s doughnuts.");
+      break;
+    case "gramophone-action":
+      console.log("User ❤️️'s music.");
+      break;
+    case "atom-action":
+      console.log("User ❤️️'s science.");
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
 });
